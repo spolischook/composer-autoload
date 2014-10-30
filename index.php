3491b307317efed9662b78d3252d4e2e995f0449
Sergey Polischuk <spolischook@gmail.com>
2014-10-30 23:02:53 +0200

    - Move model to Model namespace - Simplify front controller

--- index.php
+++ index.php
@@ -4,13 +4,9 @@ require_once 'vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Spolischook\Article;
 
 $request = Request::createFromGlobals();
 
-$article = new Article();
-$article->setTitle('Some title');
-
-$response = new Response($article->getTitle(), 200, array('Content-Type' => 'text/plain'));
+$response = new Response('Hello World!', 200, array('Content-Type' => 'text/plain'));
 $response->send();
 
