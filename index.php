5c1e94ca32c8a7786068d9b023935fdb49cdc5b9
Sergey Polischuk <spolischook@gmail.com>
2014-10-31 11:17:06 +0200

    Removed unnessesory request object

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
 
 $response = new Response('Hello World!', 200, array('Content-Type' => 'text/plain'));
 $response->send();
-
